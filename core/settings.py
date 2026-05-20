99ee9e3ef475d9efd659cb92a1a9af016d78f9df
Iasmin Borba <icb2@ic.ufal.br>
2026-05-20 08:39:13 -0300

    fix: corrige erro de cors

--- core/settings.py
+++ core/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 # MIDDLEWARE
 # ========================
 MIDDLEWARE = [
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -87,6 +86,7 @@ DATABASES = {"default": dj_database_url.parse(config("DATABASE_URL"))}
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8081",
     "http://127.0.0.1:8081",
+    "https://unibicos.vercel.app",
 ]
 # ========================
 # PASSWORD VALIDATION
